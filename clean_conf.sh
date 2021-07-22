71ecb1202044328966744f1c79988208979f0c87
Diego <diego.camarmas.alonso@gmail.com>
2021-07-22 16:12:11 +0200

    Initial commit

---
+++ clean_conf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -f xpn.conf.xml
+
+echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" >> xpn.conf.xml
+echo "<xpn_conf>" >> xpn.conf.xml
+echo "<partition>" >> xpn.conf.xml
+echo "</partition>" >> xpn.conf.xml
+echo "</xpn_conf>" >> xpn.conf.xml

99016c3aa61d5eaecfc6576676a5507dd531b862
Diego Camarmas <dcamarma@inf.uc3m.es>
2024-06-06 18:29:36 +0200

    3.0.0: initial release

--- docker/dockerstart.sh
+++ docker/dockerstart.sh
@@ -2,22 +2,22 @@
 #set -x
 
 #
-#  Copyright 2019-2023 Saul Alonso Monsalve, Diego Camarmas Alonso, Felix Garcia Carballeira, Jose Rivadeneira Lopez-Bravo, Alejandro Calderon Mateos,
+#  Copyright 2019-2024 Alejandro Calderon Mateos, Felix Garcia Carballeira, Diego Camarmas Alonso, Jose Rivadeneira Lopez-Bravo, Dario Muñoz Muñoz
 #
-#  This file is part of LAB proyect.
+#  This file is part of XPN-Docker proyect.
 #
-#  LAB is free software: you can redistribute it and/or modify
+#  XPN-Docker is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Lesser General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  U22 is distributed in the hope that it will be useful,
+#  XPN-Docker is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Lesser General Public License for more details.
 #
 #  You should have received a copy of the GNU Lesser General Public License
-#  along with U22.  If not, see <http://www.gnu.org/licenses/>.
+#  along with XPN-Docker.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 
@@ -45,4 +45,3 @@ if [ $status -ne 0 ]; then
      echo "Failed to start sshd: $status"
      exit $status
 fi
-
